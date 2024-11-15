36b06835a1baf3f528c1c608777fc99e70c97704
afifalhauzan <afifalhauzan2@gmail.com>
2024-11-15 21:13:11 +0700

    add laravel

---
+++ laravelMySQL1.sql
@@ -0,0 +1,14 @@
+create database dblaravel1;
+
+use dblaravel1;
+
+select * from dblaravel1;
+
+show tables;
+
+select * from users;
+
+insert into users(name, email)
+values ('Adit', 'adit@gmail.com')
+	   ('Ucup', 'ucup@gmail.com'),
+;
